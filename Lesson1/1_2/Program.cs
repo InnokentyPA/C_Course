f1f97000ef4f330a0a95ae17a0aa22e7b6b911fd
innokenty <eazy-mf-e@mail.ru>
2022-07-11 02:28:15 +0300

    Tusk 1-2

---
+++ Lesson1/1_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b)
+Console.WriteLine("True");
+else Console.WriteLine("Folse");
