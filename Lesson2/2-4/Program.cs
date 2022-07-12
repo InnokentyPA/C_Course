ccef99ad066846b0c2f6bda10c575d58e644ee0f
innokenty <eazy-mf-e@mail.ru>
2022-07-13 00:55:30 +0300

    2-4

---
+++ Lesson2/2-4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+void krat (int N)
+{
+    if (N % 7 == 0 && N % 23 == 0)
+    Console.WriteLine ("Yes");
+    else
+    {
+        Console.WriteLine ($"No");
+    }
+}
+int n = int.Parse(Console.ReadLine());
+
+krat (n);
