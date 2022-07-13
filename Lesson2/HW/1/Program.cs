e7e935cf5b8e9457b8566c69e937f3bea59d98c9
innokenty <eazy-mf-e@mail.ru>
2022-07-13 04:07:10 +0300

    124.1

--- Lesson2/HW/1/Program.cs
+++ Lesson2/HW/1/Program.cs
@@ -1,6 +1,5 @@
 ﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Write a number: ");
 int a = int.Parse(Console.ReadLine());
-int q = 0;
-q = (a / 10);
+int q = (a / 10);
 Console.WriteLine(q % 10);
