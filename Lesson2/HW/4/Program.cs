4683fe4dd6558936b50dc5afebb1c169c07c8e01
innokenty <eazy-mf-e@mail.ru>
2022-07-13 03:48:01 +0300

    124

--- Lesson2/HW/4/Program.cs
+++ Lesson2/HW/4/Program.cs
@@ -2,18 +2,10 @@
 Console.WriteLine("Write a number: ");
 int a = int.Parse(Console.ReadLine());
 if (a == 6)
-Console.WriteLine("Выходной");
+Console.WriteLine("Да");
 else if (a == 7)
-Console.WriteLine("Выходной");
-else if (a == 1)
-Console.WriteLine("Будни");
-else if (a == 2)
-Console.WriteLine("Будни");
-else if (a == 3)
-Console.WriteLine("Будни");
-else if (a == 4)
-Console.WriteLine("Будни");
-else if (a == 5)
-Console.WriteLine("Будни");
-else
-Console.WriteLine("Error");
+Console.WriteLine("Да");
+else if (a >= 8)
+Console.WriteLine("Не существует");
+else 
+Console.WriteLine("Нет");
