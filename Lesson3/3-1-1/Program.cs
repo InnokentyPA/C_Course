1de1c631b5444f5906b75434c9e9899e93485b90
innokenty <eazy-mf-e@mail.ru>
2022-07-17 23:47:05 +0300

    3-1-1

---
+++ Lesson3/3-1-1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 1: Напишите программу, которая по 
+//заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+void Quarters(int a)
+{
+    if (a == 1)
+        Console.WriteLine("x > 0, y > 0");
+    else if (a == 2)
+        Console.WriteLine("x < 0 && y > 0");
+    else if (a == 3)
+        Console.WriteLine("x < 0 && y < 0");
+    else if (a == 4)
+        Console.WriteLine("x > 0 && y < 0");
+    else
+        Console.WriteLine("Error");
+}
+
+Quarters(1);
+Quarters(2);
+Quarters(3);
+Quarters(4);
+Quarters(-9);
