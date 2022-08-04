143c8ef42e733ba4ff17b1fe4e4fc050cd3684cc
innokenty <eazy-mf-e@mail.ru>
2022-08-04 08:15:12 +0300

    4-1

---
+++ Lesson4/4-1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+void Length(int a)
+{
+    string num = a.ToString();
+    Console.WriteLine(num.Length);
+}
+Length(1670);
+//double Digit(double a,double b)
+//{
+//return((Math.Floor(a/(Math.Pow(10,Math.Floor(Math.Log10(a))-b+1)))%10));
+//}
