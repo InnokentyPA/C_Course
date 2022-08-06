43333cb77bbae8ff900d12f1e3201d2a1e56705e
innokenty <eazy-mf-e@mail.ru>
2022-08-06 06:13:54 +0300

    HW 5-3

--- Lesson5/HW/5-3/Program.cs
+++ Lesson5/HW/5-3/Program.cs
@@ -1,2 +1,39 @@
 ﻿//Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+    return arr;
+}
+int NiceNum(int[] arr)
+{
+    int size = arr.Length;
+    int OTBET = 0;
+    int max = arr[0];
+    int min = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+        else if (arr[i] > max) max = arr[i]; 
+    }
+    return OTBET = max - min;
+}
+int[] arr_1 = MassNums(9);
+Print(arr_1);
+Console.WriteLine(NiceNum(arr_1));
