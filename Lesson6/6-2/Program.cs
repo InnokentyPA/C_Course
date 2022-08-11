fd7ad2e3382af25cd7876c9930e6837ddfe711a4
innokenty <eazy-mf-e@mail.ru>
2022-08-11 06:43:12 +0300

    6-2

--- Lesson6/6-2/Program.cs
+++ Lesson6/6-2/Program.cs
@@ -3,3 +3,14 @@
 //45 -> 101101
 //3 -> 11
 //2 -> 10
+string ChengeNum(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result;
+}
+Console.WriteLine(ChengeNum(12));
