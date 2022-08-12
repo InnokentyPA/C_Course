66a1b676d24769ed8644c5fae5f6fd951e7964d6
innokenty <eazy-mf-e@mail.ru>
2022-08-12 06:16:13 +0300

    HW6

---
+++ Lesson6/HW/6-1/Program.cs
@@ -0,0 +1,14 @@
+﻿//1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите кол-во чисел: ");
+int num = int.Parse(Console.ReadLine());
+int a = 0;
+int Pos = 0;
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine("Введите число: ");
+    a = int.Parse(Console.ReadLine());
+    if (a > 0) Pos++;
+}
+Console.WriteLine($" Кол-во чисел больше 0 -> {Pos} ");
---
+++ Lesson6/HW/6-3/Program.cs
@@ -0,0 +1,35 @@
+﻿//3: Напишите программу, которая будет создавать копию заданного массива(вещественные числа) с помощью поэлементного копирования.
+void Print(double[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+double[] MassNums(int size)
+{
+    double[] arr = new double[size];
+    Random n_new = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Math.Round(n_new.NextDouble() * (10 + 12) - 10, 2);
+    }
+    return arr;
+}
+void Copy(double[] arr_1)
+{
+    double[] copy = new double[arr_1.Length];
+    for (int i = 0; i < arr_1.Length; i++)
+    {
+        copy[i] = arr_1[i];
+    }
+
+    Console.WriteLine(String.Join(", ", copy));
+}
+
+double[] arr_1 = MassNums(6);
+Print(arr_1);
+Copy(arr_1);
