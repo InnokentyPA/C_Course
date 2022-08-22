57e9ef4d7fbb41da1e36875bc3b5abcadaa40ec4
innokenty <eazy-mf-e@mail.ru>
2022-08-22 03:58:59 +0300

    HW9

--- Lesson9/HW/9-2/Program.cs
+++ Lesson9/HW/9-2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumNum(int N, int M)
+{
+    if (N == M) return M;
+    return SumNum(N, M - 1) + M;
+
+}
+Console.WriteLine(SumNum(1, 15));
+Console.WriteLine(SumNum(4, 8));
